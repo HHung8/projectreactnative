bdfe7e1e0b509403975a27f9a0cd2032b726fa71
hung <hung@hungs-MacBook-Air.local>
2026-05-18 17:49:38 +0700

    fix: add rnclient as normal folder

---
+++ rnclient/app/(tabs)/index.tsx
@@ -0,0 +1,75 @@
+import ListHeading from "@/components/ListHeading";
+import SubScriptionCard from "@/components/SubScriptionCard";
+import UpcomingSubcriptionCard from "@/components/UpcomingSubcriptionCard";
+import { HOME_BALANCE, HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import { styled } from "nativewind";
+import { useState } from "react";
+import { FlatList, Image, Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import "../../global.css";
+
+const SafeAreaView = styled(RNSafeAreaView);
+export default function App() {
+  const [expandedSubscrptionId, setExpandedSubscriptionId] = useState<string | null>(null);
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+        <FlatList
+          ListHeaderComponent={() => (
+            <>
+              <View className="home-header">
+                <View className="home-user">
+                  <Image source={images.avatar} className="home-avatar" />
+                  <Text className="home-user-name">{HOME_USER.name}</Text>
+                </View>
+                <Image source={icons.add} className="home-add-icon" />
+              </View>
+
+              <View className="home-balance-card">
+                <Text className="home-balance-label">Balance</Text>
+                <View className="home-blance-row">
+                  <Text className="home-balance-amount">
+                    {formatCurrency(HOME_BALANCE.amount)}
+                  </Text>
+                  <Text className="home-balance-date">
+                    {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+                  </Text>
+                </View>
+              </View>
+
+              <View className="mb-5">
+                <ListHeading title="Upcoming Subscriptions" />
+                <FlatList
+                  data={UPCOMING_SUBSCRIPTIONS}
+                  renderItem={({ item }) => (<UpcomingSubcriptionCard {...item} />)}
+                  keyExtractor={(item) => item.id}
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                  ListEmptyComponent={<Text className="home-empty-state">No upcoming subscriptions</Text>}
+                />
+              </View>
+
+              <ListHeading title="All Subscriptions" />
+            </>
+         )}
+          data={HOME_SUBSCRIPTIONS}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <SubScriptionCard
+              {...item}
+              expanded={expandedSubscrptionId === item.id}
+              onPress={() => setExpandedSubscriptionId((currentId) => currentId === item.id ? null : item.id)}
+            />
+          )}
+          extraData={expandedSubscrptionId}
+          ItemSeparatorComponent={() => <View className="h-4" />}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={<Text className="home-empty-state">No subscriptions found</Text>}
+          contentContainerClassName="pb-30"
+        />
+    </SafeAreaView>
+  );
+}
---
+++ rnclient/app/onbroading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const onbroading = () => {
+  return (
+    <View>
+      <Text>onbroading</Text>
+    </View>
+  )
+}
+
+export default onbroading
